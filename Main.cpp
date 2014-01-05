d8f8a2839078b94101c8d638e2af9a343d3e9a42
itair <itair@163.com>
2014-01-05 13:48:57 +0800

    第一题

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,5 @@
 /*-----------------------------------------
-//   C++ Primer Plus 课后习题 p298 8.8
+//   C++ Primer Plus 课后习题 p654 15.8
 //   by itAir
 //   Try to use Google C++ programing style   
 ----------------------------------------- */    
@@ -8,6 +8,7 @@
 #include "Inline.h"
 #include <iostream>
 #include <string>
+#include "tvfm.h"   // only change to use_tv.cpp
 
 using namespace std;
 
@@ -17,7 +18,34 @@ void main()
   longline();
 
   longline(1); //1  
-  
+  using std::cout;
+  Tv s27;
+  cout << "Initial settings for 27\" TV:\n";
+  s27.settings();
+  s27.onoff();
+  s27.chanup();
+  cout << "\nAdjusted settings for 27\" TV:\n";
+  s27.settings();
+
+  Remote grey;
+  grey.set_chan(s27, 10);
+  grey.volup(s27);
+  grey.volup(s27);
+  cout << "\n27\" settings after using remote:\n";
+  s27.settings();
+
+  Tv s32(Tv::On);
+  s32.set_mode();
+  grey.set_chan(s32,28);
+  cout << "\n32\" settings:\n";
+  s32.settings();
+
+
+
+
+
+
+
   longline();
   system("pause");
 }
